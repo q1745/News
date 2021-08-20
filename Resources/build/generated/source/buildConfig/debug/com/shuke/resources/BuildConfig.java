3a1bd6521bf9df48f0cce52eb06311ef806b990e
shukeCyp <3536815334@qq.com>
2021-08-20 16:12:07 +0800

    11

---
+++ Resources/build/generated/source/buildConfig/debug/com/shuke/resources/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.shuke.resources;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String LIBRARY_PACKAGE_NAME = "com.shuke.resources";
+  public static final String BUILD_TYPE = "debug";
+}
