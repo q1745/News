f4df4706a15027201a58d67a18e19bc7a9c0d3e9
shukeCyp <3536815334@qq.com>
2021-08-20 15:29:46 +0800

    完善搜索

---
+++ homepage/src/main/java/com/shuke/homepage/customview/FluidViewAdapter.kt
@@ -0,0 +1,8 @@
+package com.shuke.homepage.customview
+
+/**
+ *   @Author:YaPeng
+ *   @Date:2021/8/20
+ *   @Email:3536815334@qq.com
+ */class FluidViewAdapter {
+}
---
+++ homepage/src/main/java/com/shuke/homepage/search/repository/SearchRepository.kt
@@ -0,0 +1,8 @@
+package com.shuke.homepage.search.repository
+
+/**
+ *   @Author:YaPeng
+ *   @Date:2021/8/20
+ *   @Email:3536815334@qq.com
+ */class SearchRepository {
+}
